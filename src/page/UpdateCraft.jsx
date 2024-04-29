53f76acda05f91e645fb45e2dc8d92ced60623ee
mmirazz <mehedimiraz900@gmail.com>
2024-04-29 21:36:47 +0600

    update craft page editing

--- src/page/UpdateCraft.jsx
+++ src/page/UpdateCraft.jsx
@@ -1,15 +1,58 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 
 const UpdateCraft = () => {
     const updateCraft =useLoaderData()
-    const {image, itemName, subcategoryName, price, shortDescription, rating, customization, processingTime, stockStatus, userEmail, userName}= updateCraft
+    const {_id, image, itemName, subcategoryName, price, shortDescription, rating, customization, processingTime, stockStatus}= updateCraft
+
+    const handelUpdateArtAndCraft = event => {
+        event.preventDefault()
+      
+
+        const form = event.target;
+       
+        const image = form.image.value;
+        const itemName = form.itemName.value;
+        const subcategoryName = form.subcategoryName.value;
+        const price = form.price.value;
+        const shortDescription = form.shortDescription.value;
+        const rating = form.rating.value;
+        const customization = form.customization.value;
+        const processingTime = form.processingTime.value;
+        const stockStatus = form.stockStatus.value;
+        // const userEmail = form.userEmail.value;
+        // const userName = form.userName.value;
+        event.target.reset()
+        const UpdatedArtAndCraft = {image, itemName, subcategoryName, price, shortDescription, rating, customization, processingTime, stockStatus};
+        console.log(UpdatedArtAndCraft);
+
+        fetch(`http://localhost:5000/artCraft/${_id}`,{
+            method: 'PUT',
+            headers: {
+                'content-type': "application/json"
+            }, 
+            body: JSON.stringify(UpdatedArtAndCraft)
+        } )
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'success',
+                    text: 'Item updated successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                  })
+            }
+        })
+    }
     return (
         <div className="my-24 p-8 bg-[#F4F3F0] rounded-lg">
 
-        <h2 className="text-center  my-6 text-2xl md:text-3xl lg:text-4xl font-bold text-green-500">Add Art &Craft</h2>
-        <p className=" text-lg md:text-xl font-medium text-center mb-12 text-green-500">Unleash your creativity through a symphony of colors and textures, crafting beauty with your own hands. Explore the boundless realm of imagination, where every stroke tells a story and every creation reflects your unique vision.</p>
-     <form onSubmit={handelAddArtAndCraft} >
+        <h2 className="text-center  my-6 text-2xl md:text-3xl lg:text-4xl font-bold text-green-500">Update Art &Craft</h2>
+        <p className=" text-lg md:text-xl font-medium text-center mb-12 text-green-500">A creative haven where imagination thrives, offering a diverse realm of expression through skilled hands and boundless innovation.</p>
+     <form onSubmit={handelUpdateArtAndCraft} >
 
      <div className="flex flex-col md:flex-row gap-4">
         <div className="w-full">
@@ -20,6 +63,7 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="image"
             type="text"
+            defaultValue={image}
             placeholder="Enter  craft image URL"
           />
         </div>
@@ -32,6 +76,7 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="itemName"
             type="text"
+            defaultValue={itemName}
             placeholder="Enter craft name"
           />
         </div>
@@ -45,6 +90,7 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="subcategoryName"
             type="text"
+            defaultValue={subcategoryName}
             placeholder="Enter craft subcategory_Name"
           />
         </div>
@@ -57,6 +103,7 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="price"
             type="text"
+            defaultValue={price}
             placeholder="Enter craft Price"
           />
         </div>
@@ -69,6 +116,7 @@ const UpdateCraft = () => {
           className="w-full p-3 rounded-md"
           name="shortDescription"
           type="text"
+          defaultValue={shortDescription}
           placeholder="Enter craft short description"
         />
       </div>
@@ -81,6 +129,7 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="rating"
             type="text"
+            defaultValue={rating}
             placeholder="Enter craft rating"
           />
         </div>
@@ -93,6 +142,7 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="customization"
             type="text"
+            defaultValue={customization}
             placeholder="Enter craft customization"
           />
         </div>
@@ -106,6 +156,7 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="processingTime"
             type="text"
+            defaultValue={processingTime}
             placeholder="Enter craft processing_time"
           />
         </div>
@@ -118,11 +169,12 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="stockStatus"
             type="text"
+            defaultValue={stockStatus}
             placeholder="Enter craft stock_status"
           />
         </div>
       </div>
-      <div className="flex flex-col md:flex-row gap-4 my-4">
+      {/* <div className="flex flex-col md:flex-row gap-4 my-4">
         <div className="w-full">
           <p className="text-xl md:text-2xl lg:text-3xl font-bold mb-2 text-amber-600 ">
           User Email .
@@ -131,6 +183,7 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="userEmail"
             type="email"
+            defaultValue={userEmail}
             placeholder="Enter craft user email"
           />
         </div>
@@ -143,12 +196,13 @@ const UpdateCraft = () => {
             className="w-full p-3 rounded-md"
             name="userName"
             type="text"
+            defaultValue={userName}
             placeholder="Enter craft User Name"
           />
         </div>
-      </div>
+      </div> */}
 
-      <input className="btn flex mx-auto mt-6 w-full text-xl md:text-2xl bg-green-600 text-white " type="submit" value="Add" />
+      <input className="btn flex mx-auto mt-6 w-full text-xl md:text-2xl bg-green-600 text-white " type="submit" value="Update" />
      </form>
     </div>
     );
